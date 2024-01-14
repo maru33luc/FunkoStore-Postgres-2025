bbb0d1bd7daf6b732bee8a8c8f5012084d533f5e
maru33luc <maru33luc@gmail.com>
2024-01-14 18:34:52 -0300

    [FEAT] prod environments domain render

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,11 +1,15 @@
+// const domain = 'https://copia-deploy-funkostoreutn-production.up.railway.app/';
+// const domain = 'http://localhost:3000/';
+const domain = 'https://funkostore.onrender.com/';
+
 export const environments = {
-    urlFunkosData: 'http://localhost:3000/fk',
-    urlUsersData: 'http://localhost:3000/users',
-    urlCartData: 'http://localhost:3000/carts',
+    urlFunkosData: domain + 'fk',
+    urlUsersData: domain + 'users',
+    urlCartData: domain + 'carts',
     urlAPITolkien: 'https://the-one-api.dev/v2',
     tokenAPITolkien: 'wZyG4hBq4wxlOy_YH1hL',
     urlAPIStarWars: 'https://starwars-databank-server.vercel.app/api/v1',
     urlAPIPotter: 'https://potterhead-api.vercel.app',
-    urlPokemonJson: 'http://localhost:4000',
+    urlPokemonJson: '',
     urlFormsPree: 'https://formspree.io/f/xknlabrk'
 }
