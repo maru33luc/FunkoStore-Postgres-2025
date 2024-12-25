831830bf47108eb1792988c0d6378c4a5cbce61b
maru33luc <maru33luc@gmail.com>
2024-12-25 20:41:05 -0300

    Add render configuration and enhance error handling in server app

--- server/app.js
+++ server/app.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
+const path = require('path');  // Asegúrate de importar path
 const port = process.env.PORT || 3000;
 const cors = require('cors');
+// const session = require('express-session');
+// const cookieParser = require('cookie-parser');
 
+// Configurar CORS
 app.use(cors({
-  origin: ['http://localhost:4200', 'https://funkostore-postgres-2025.onrender.com', 'https://funko-store-seven.vercel.app','https://funkostore.onrender.com' ],
+  origin: ['http://localhost:4200', 'https://funkostore-postgres-2025.onrender.com','https://funko-store-seven.vercel.app'],
   credentials: true
 }));
 
 app.options('*', cors()); // Permite preflight requests para todas las rutas
 
+// Manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// Configurar rutas y middlewares
 const db = require('./data/db');
 const funkoRoutes = require('./src/routes/funkoRoutes');
 const usersRoutes = require('./src/routes/usersRoutes');
@@ -17,20 +28,26 @@ const cartRoutes = require('./src/routes/cartRoutes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
 
 // Servir archivos estáticos desde la carpeta 'dist', en la raíz del sitio
 app.use(express.static(path.join(__dirname, 'dist', 'funko-store')));
 
-// Asegurarse de que todas las rutas que no sean de la API redirijan a 'index.html'
+// Rutas de la API
+app.use('/fk', funkoRoutes);
+app.use('/users', usersRoutes);
+app.use('/carts', cartRoutes);
+
+// Ruta de fallback para manejar rutas Angular (evitar errores 404 al recargar la página)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'funko-store', 'index.html'));
 });
 
-
+// Conexión a la base de datos
 const conexionDB = async () => {
   try {
     await db.authenticate();
-    console.log("Conexión exitosa");
+    console.log("conexion exitosa");
   } catch (error) {
     console.log(error);
   }
@@ -38,11 +55,5 @@ const conexionDB = async () => {
 
 app.listen(port, () => {
   conexionDB();
-  console.log(`API_BACK escuchando en http://localhost:${port}`);
+  console.log(`API_BACK listening at http://localhost:${port}`);
 });
-
-app.use('/fk', funkoRoutes);
-app.use('/users', usersRoutes);
-app.use('/carts', cartRoutes);
-
-
